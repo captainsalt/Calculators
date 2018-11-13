b387f484ce457832b85df588fe610fba5668052a
David Wright <dontbothertheking@gmail.com>
2018-11-12 18:12:45 -0600

    Move bracket logic to a seperate function

--- src/InfixNotation/Program.fs
+++ src/InfixNotation/Program.fs
@@ -19,34 +19,35 @@ let getBinaryOp = function
 | "%" -> (%)
 | _ as op -> failwith (sprintf "Invalid operation: %s" op)
     
+/// Calculates an expression
 let calculate = function 
 | Regex expressionPattern [num1; op; num2] -> sprintf "%i" (getBinaryOp op (int num1) (int num2))
 | _ as invalid -> failwithf "Invalid expression: %s" invalid
 
-/// Gets the innermost expression in brackets from left to right
-let rec solve (expression: string) = 
-    match expression with 
-    | Regex solvedPattern [solution] -> 
-        solution
-    | _ ->
-        let someEndIndex = expression |> Seq.tryFindIndex (fun c -> c = ')')
+/// Gets and solves expressions in brackets from left to right
+let rec solveBrackets (expression: string) = 
+    let someEndIndex = expression |> Seq.tryFindIndex (fun c -> c = ')')
 
-        match someEndIndex with
-        | Some endIndex ->
-            let someStartIndex = expression.[0..endIndex - 1] |> Seq.tryFindIndexBack (fun c -> c = '(')
+    match someEndIndex with
+    | Some endIndex ->
+        let someStartIndex = expression.[0..endIndex - 1] |> Seq.tryFindIndexBack (fun c -> c = '(')
 
-            match someStartIndex with 
-            | Some startIndex -> 
-                let expressionFragment = expression.[startIndex + 1.. endIndex - 1]
-                let solution = calculate expressionFragment
-                let newExpression = expression.Replace(sprintf "(%s)" expressionFragment, solution)
-                solve newExpression
-            | None -> failwith "Cannot solve expression. Bracket mismatch"
-        | None -> "Error" // No ending brackets found check for opening brackets or solve normally?
+        match someStartIndex with 
+        | Some startIndex -> 
+            let expressionFragment = expression.[startIndex + 1.. endIndex - 1]
+            let solution = calculate expressionFragment
+            let newExpression = expression.Replace(sprintf "(%s)" expressionFragment, solution)
+            solveBrackets newExpression
+        | None -> failwith "Cannot solve expression. Bracket mismatch"
+    | None -> expression
+
+let rec solve = function
+| Regex solvedPattern [solution] -> solution
+| _ as expression -> solveBrackets expression
 
 [<EntryPoint>]
 let main argv =
     let solution = argv |> String.concat " " |> solve
     printfn "%s" (solve "(((1 + 7) * 8) + 6)")
-    0 // return an integer exit code
+    0
 
